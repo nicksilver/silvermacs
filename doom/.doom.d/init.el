10995d7ccd228674d63d2b633c243b183a10c749
Nick Silverman <nick.silverman@waterplan.com>
2022-11-06 09:38:19 -0700

    practicing elisp

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -139,7 +139,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +noter +jupyter +dragndrop +pretty +journal +ipython)               ; organize your plain life in plain text
+       (org +roam2 +noter +jupyter +dragndrop +pretty +journal +ipython +pandoc)               ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
