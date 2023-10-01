6f93e90302b09d37af4f81332ce345304fc49896
nicksilver <nick.silverman11@gmail.com>
2023-10-01 10:35:03 -0600

    Small edits to sync

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -73,7 +73,7 @@
   (setq vterm-shell "bash")
   (setq vterm-max-scrollback 10000))
 
-;; ESS settings ================================================
+;; ESS settings
 (use-package ess-r-mode
   :bind
   (:map ess-r-mode-map
